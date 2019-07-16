8041f992e8c7f614055459adf8c2fe6832949c42
christiansallen <christiansallen@gmail.com>
2019-07-16 12:39:13 -0600

    passed data down to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
+import {Specials} from "./components/ButtonComponents/SpecialButtons/Specials";
+import {Numbers} from "./components/ButtonComponents/NumberButtons/Numbers";
+import {Operators} from "./components/ButtonComponents/OperatorButtons/Operators";
+import {Display} from "./components/DisplayComponents/Display";
+
+
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
@@ -13,11 +19,41 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+  const [displayValue, setDisplayValue] = useState(0)
+  const [firstArray, setFirstArray] = useState([])
+  
+
+  //FUNCTIONS
+  const handleClick = (name) => {
+      handleOperation(name);
+      storeNumber(name);
+      setDisplayValue(name);
+  }
+
+  const storeNumber = (name) => {
+    const array = [...firstArray]
+    array.push(name);
+    setFirstArray(array);
+  }
+
+  const handleOperation = (name) => {
+    const operation = {
+      '+': ((x, y)=> x+y),
+      '-': ((x, y)=> x-y)
+    }
+    console.log(operation[name]);
+  }
+
+  console.log(firstArray)
+
   return (
     <div className="container">
       <Logo />
       <div className="App">
-        {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+      <Display displayValue = {displayValue}/>
+      <Specials handleClick={handleClick}/>
+      <Numbers handleClick={handleClick}/>
+      <Operators handleClick={handleClick}/>
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
+import {NumberButton} from "./NumberButton";
 
 //Import your array data to from the provided data file
+import {numbers} from "./../../../data"
 
-const Numbers = () => {
+
+export const Numbers = (props) => {
   // STEP 2 - add the imported data to state
+  const [numbersData, setNumbersData] = useState(numbers)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+        numbersData.map(number => {
+          return <NumberButton name={number} handleClick={props.handleClick}/>
+        })
+      }
     </div>
   );
 };
